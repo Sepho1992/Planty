ff0390cdca6c2283c2759a20052b05cb15e04aa3
Mondin <sephora.mondin@gmail.com>
2024-02-23 00:21:22 +0100

    site complet

--- wp-content/themes/PlantyChild/functions.php
+++ wp-content/themes/PlantyChild/functions.php
@@ -9,11 +9,12 @@ function theme_enqueue_styles()
 
 <?php add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
  function add_admin_link( $items, $args ) 
-{ if (is_user_logged_in() && $args->theme_location == 'menu-1-60ff904'){
+{ if (is_user_logged_in() && $args->menu_class == 'hfe-menu-item'){
  $items .= '<li id="menu-item-319" class="menu-item menu-item-type-custom menu-item-object-custom parent hfe-creative-menu"><a href="'. 
 get_admin_url() .' class="hfe-menu-item">Admin</a></li>'; 
 }
  return $items; } ?>
+
 
 
 
